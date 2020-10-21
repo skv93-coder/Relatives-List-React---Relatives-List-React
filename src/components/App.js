ffffcf5f109690640a3b1f72abda7078d84ab9ea
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-10-21 14:13:32 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,10 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 class App extends Component {
-  // relatives = ["Arpit", "Btkers", "Cbdul", "Szzz", "Werd"];
+  relatives = ["Arpit", "Btkers", "Cbdul", "Szzz", "Werd"];
 
   render() {
-    relatives = ["Arpit", "Btkers", "Cbdul", "Szzz", "Werd"];
+    //relatives = ["Arpit", "Btkers", "Cbdul", "Szzz", "Werd"];
 
     return (
       <ol key="relativeList">
